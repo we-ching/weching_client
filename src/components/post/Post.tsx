815822bdd0c45fb8517a6a94a56ee2ce84003a4a
Hwang Jung Hyeon <1004wipi@gmail.com>
2022-12-15 17:49:02 +0900

    feat: 게시물 내용 json-server로 전송 기능 구현

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -3,47 +3,53 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './Post.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-// - [ ] 게시하기 버튼을 통해 title, body 서버로 전송
+// - [ ] 게시하기 버튼을 통해 token header(누가쓴 글인지 확인해야 하므로), body 서버로 전송
+// 올린 게시물의 내용이 redux를 통해 전역적으로 관리되야하는 상태가 맞는건가요?
 
 const Body = () => {
+  const [body, setBody] = useState({
+    contents: '',
+  });
+  const submitHandler = async () => {
+    await axios.post(`http://localhost:4000/posts`, {
+      contents: body,
+    });
+  };
   return (
-    <CKEditor
-      editor={ClassicEditor}
-      config={{
-        placeholder: '수정이 불가하므로 신중한 작성바랍니다.',
-        toolbar: ['bold', 'italic', 'numberedList', 'bulletedList'],
-      }}
-      data="" //  서버로 전달
-      onReady={(editor: any) => {
-        // You can store the "editor" and use when it is needed.
-        console.log('Editor is ready to use!', editor);
-      }}
-      onChange={(event: any, editor: any) => {
-        const data = editor.getData();
-        console.log({ event, editor, data });
-      }}
-      onBlur={(event: any, editor: any) => {
-        console.log('Blur.', editor);
-      }}
-      onFocus={(event: any, editor: any) => {
-        console.log('Focus.', editor);
-      }}
-    />
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          placeholder: '수정이 불가하므로 신중한 작성바랍니다.',
+          toolbar: ['bold', 'italic', 'numberedList', 'bulletedList'],
+        }}
+        data="" //  서버로 전달
+        onReady={(editor: any) => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event: any, editor: any) => {
+          const data = editor.getData();
+          setBody(data);
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event: any, editor: any) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event: any, editor: any) => {
+          console.log('Focus.', editor);
+        }}
+      />
+      <button onClick={submitHandler}>올리기</button>
+    </>
   );
 };
 
 export const Post = () => {
-  const submitHandler = () => {
-    axios.post(`http://localhost:3232/posts`);
-  };
-  return (
-    <>
-      <Body />
-      <button onClick={submitHandler}>칭찬해줘잉</button>
-    </>
-  );
+  return <Body />;
 };
 
 const Title = styled.input`
