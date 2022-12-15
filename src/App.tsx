db1639d497bea4f0bac9d906198b25ef94c00aac
Hwang Jung Hyeon <1004wipi@gmail.com>
2022-12-15 12:12:08 +0900

    feat: CKEditor 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Post } from './components/post/Post';
 
 function App() {
   return (
     <div className="App">
-      
+      <Post />
     </div>
   );
 }
---
+++ src/components/post/Post.tsx
@@ -0,0 +1,45 @@
+// - [ ] 게시하기 버튼을 통해 title, body 서버로 전송
+
+import styled from 'styled-components';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const Title = () => {
+  return <input type="text" name="title" placeholder="title" />;
+};
+
+const Body = () => {
+  return (
+    <CKEditor
+      editor={ClassicEditor}
+      data="<p>Hello from CKEditor 5!</p>"
+      onReady={(editor: any) => {
+        // You can store the "editor" and use when it is needed.
+        console.log('Editor is ready to use!', editor);
+      }}
+      onChange={(event: any, editor: any) => {
+        const data = editor.getData();
+        console.log({ event, editor, data });
+      }}
+      onBlur={(event: any, editor: any) => {
+        console.log('Blur.', editor);
+      }}
+      onFocus={(event: any, editor: any) => {
+        console.log('Focus.', editor);
+      }}
+    />
+  );
+};
+
+export const Post = () => {
+  return (
+    <Wrapper>
+      <Title />
+      <Body />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: green;
+`;
