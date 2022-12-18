25b95d5beceab1f0892f9926679384510327a4b2
tangjinlog <lkg70007@gmail.com>
2022-12-18 21:37:50 +0900

    feat: 기존회원 get요청-token받기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Post from './components/post';
 import { Reply } from './components/reply/Reply';
 import GlobalStyle from './GlobalStyle';
 import { LoginForm } from './components/Login';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
--- src/components/Login/styled.tsx
+++ src/components/Login/styled.tsx
@@ -7,7 +7,6 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: gray;
   backdrop-filter: blur(10px);
 `;
 
