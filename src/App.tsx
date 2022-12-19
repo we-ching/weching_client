491f0ae321a59766943e8968d32444ae8290018c
tangjinlog <lkg70007@gmail.com>
2022-12-19 16:27:24 +0900

    refactor: 컨벤션 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Post from './components/post';
 import { Reply } from './components/reply/Reply';
+import MyPage from './components/mypage';
 import GlobalStyle from './GlobalStyle';
+import { MyPost } from './components/mypage/MyPost';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App"></div>
+      <div className="App">
+        <MyPost />
+      </div>
     </>
   );
 }
