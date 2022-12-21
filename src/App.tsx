0471c21ee7b565668293a092ba21ed15cccbeb3f
tangjinlog <lkg70007@gmail.com>
2022-12-22 00:25:20 +0900

    refactor: axios 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,17 @@ import { Route } from 'react-router-dom';
 import { MainPage } from './components/MainPage';
 import { Reply } from './components/reply/Reply';
 import { MyPage } from './components/mypage/index';
+import { MyPost } from './components/mypage/MyPost';
 import { LandingPage } from './components/landingPage/LandingPage';
 import { Post } from './components/post/Post';
-import { Alarm } from './components/MainPage/Alarm/alarm';
+import { Alarm } from './components/MainPage/Alarm/';
 import { LoginForm } from './components/Login/LoginForm';
 import { NotFound } from './components/NotFound';
+import axios from 'axios';
+
+// const baseURL = 'https://port-0-weching-53px25lbvs1fg6.gksl2.cloudtype.app';
+// axios.defaults.baseURL = baseURL;
+// axios.defaults.withCredentials = true;
 
 const App: FC = () => {
   return (
@@ -22,6 +28,7 @@ const App: FC = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<MainPage />} />
           <Route path="/myPage" element={<MyPage />} />
+          <Route path="/mypage/mypost" element={<MyPost />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/post" element={<Post />} />
           <Route path="/reply" element={<Reply />} />
