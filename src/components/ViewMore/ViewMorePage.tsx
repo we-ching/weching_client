84ff39d0a9d10c66d91c7ec1b1a7c4d172106fc9
tangjinlog <lkg70007@gmail.com>
2022-12-22 12:50:29 +0900

    viewmoerpage 수정

--- src/components/ViewMore/ViewMorePage.tsx
+++ src/components/ViewMore/ViewMorePage.tsx
@@ -3,57 +3,55 @@ import { useNavigate, Router, Route } from 'react-router-dom';
 import { Recruit } from './StaticPageRecruit';
 import * as S from './styled';
 
-
 export const ViewMore = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-
-        <S.BackGround>
-            <S.MenuContainer>
-                <S.MenuBox
-                    onClick={()=> {
-                        navigate('/home');
-                    }}
-                >
-                    공지사항
-                </S.MenuBox>
-                <S.MenuBox
-                    onClick={()=> {
-                        navigate('/home');
-                    }}
-                >
-                    랭킹
-                </S.MenuBox>
-                <S.MenuBox
-                    onClick={()=> {
-                        navigate('/home');
-                    }}
-                >
-                    FAQs
-                </S.MenuBox>
-                <S.MenuBox
-                    onClick={()=> {
-                        navigate('/home');
-                    }}
-                >
-                    친구 초대
-                </S.MenuBox>
-                <S.MenuBox
-                    onClick={()=> {
-                        navigate('/viewMore/Recruit');
-                    }}
-                >
-                    채용
-                </S.MenuBox>            
-                <S.MenuBox
-                    onClick={()=> {
-                            navigate('/viewMore/About');
-                        }}
-                >
-                    서비스 정보
-                </S.MenuBox>
-            </S.MenuContainer>
-        </S.BackGround>
-    )
-}
+  return (
+    <S.BackGround>
+      <S.MenuContainer>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          공지사항
+        </S.MenuBox>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          랭킹
+        </S.MenuBox>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          FAQs
+        </S.MenuBox>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          친구 초대
+        </S.MenuBox>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/viewmore/recruit');
+          }}
+        >
+          채용
+        </S.MenuBox>
+        <S.MenuBox
+          onClick={() => {
+            navigate('/viewmore/about');
+          }}
+        >
+          서비스 정보
+        </S.MenuBox>
+      </S.MenuContainer>
+    </S.BackGround>
+  );
+};
