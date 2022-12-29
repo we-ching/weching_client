5f1e18af1d26c18ab91a7735dfea77068047e19d
Hwang Jung Hyeon <1004wipi@gmail.com>
2022-12-29 18:25:03 +0900

    feat: 토큰 없을 시 예외처리

--- src/components/reply/RandomPost.tsx
+++ src/components/reply/RandomPost.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import * as S from './styled';
 import { getCookie } from '../Login/GoogleBtn';
 
@@ -16,9 +16,14 @@ const RandomPost = () => {
   const [post, setPost] = useState('');
   const params = useParams();
   const id = Number(params.id);
+  const navigate = useNavigate();
 
   const getPost = async () => {
     const token = getCookie('accessToken');
+    if (!token) {
+      alert('로그인 후 이용해주세요❗️');
+      return navigate('/login');
+    }
     await axios
       .get(`/api/post/${id}`, {
         headers: {
